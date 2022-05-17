cbf965e1d4191e815f22d37d02de651b75c3e010
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-05-17 22:29:19 +0500

    some-expressJS-basics

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,12 @@
 const express = require('express')
 const router = express.Router()
+router.use(logger)
 router.get('/',(req,res)=>{
     res.send('User List')
   })
   
-
-  router.route("/:id").get((req,res)=>{
-    res.send('User New  Form')
-}).put((req,res)=>{
-    res.send('User New  Form')
-}).delete((req,res)=>{
-    res.send('User New  Form')
-})
-
   router.post('/',(req,res)=>{
     res.send('Create User')
-})
-
-router.get('/new',(req,res)=>{
-      res.send('User New  Form')
-  })
-router.get('/:id',(req,res)=>{
-    req.params.id
-    res.send(`User get with ID ${req.params.id}`)
 })
 router.put('/',(req,res)=>{
     res.send('Create User')
@@ -31,6 +15,43 @@ router.put('/',(req,res)=>{
 router.delete('/',(req,res)=>{
     res.send('Create User')
 })
+
+
+  router.route("/:id").get((req,res)=>{
+    res.send(`User get with ID ${req.params.id}`)
+    console.log(req.user)
+}).put((req,res)=>{
+    res.send(`User get with ID ${req.params.id}`)
+}).delete((req,res)=>{
+    res.send(`User get with ID ${req.params.id}`)
+})
+
+router.get('/new',(req,res)=>{
+      res.send('User New  Form')
+  })
+
+
+// it is going to run any tine when it find the param it matches the name you find in
+const users =[{name:"tuba"},{name:"shehla"}]
+
+router.param("id",(req,res,next,id)=>{
+    req.user= users[id]
+    // this find that params and run the code
+    // but it does not actually run anyother code
+    // untill we call this function next
+// bcz the param in here is actually work as a middle layer
+next() // when we call this then the actually reponse will return to user
+// means the above get put methods codes
+
+console.log(id)
+})
+// middleware - we mostly use next when we work on middleware
+
+function logger(req,res,next){
+    console.log(req.originalUrl)
+    next()
+    }
+    
 
 
 module.exports= router
--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
 app.set("view engine","ejs")
+//app.use(logger) 
+// define middleware at the top of your file if you want it to use everywhere
+// bcz middleware work as top to bottom
+// if we dont want it to use everywhere use it individually on end points
 
 
-app.get('/',(req,res)=>{
+app.use(express.static("public"))
+
+// can set directly with endpoints
+// remaining code will run after this logger function
+// can add multiple logger files as well
+
+app.get('/',logger,(req,res)=>{
     console.log('here')
   //  res.sendStatus(500).send('hello error tuba')
  //res.sendStatus(500).json({message:'hello error tuba'})
  //res.download("server.js")
     //res.send('hi')
-    res.render("index",{text:"world"})
+
+    // static way - to access files 
+  //  res.render("index",{text:"world"})
 })
 
 
 const userRouters= require('./routes/users')
 app.use("/users",userRouters)
+
+
+// middleware - we mostly use next when we work on middleware
+
+function logger(req,res,next){
+    console.log(req.originalUrl)
+    next()
+    }
+    
+    
 app.listen(3000)
